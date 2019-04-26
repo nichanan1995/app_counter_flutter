839cbbf618f8604d749193e1ddd487efd3d0cd57
nichanan <nichanan.j@sunteen.co.th>
2019-04-26 10:23:22 +0700

    add_trans

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,6 @@
-// import 'dart:async';
-// import 'package:flutter/material.dart';
-// import 'package:qrcode_reader/qrcode_reader.dart';
-// import 'dart:convert';
-// import 'package:http/http.dart' show get;
-// void main() {
-//   runApp(new MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return new MaterialApp(
-//       title: 'QRCode Tracking NO.',
-//       home: new MyHomePage(),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   //final Widget child;
-
-//   MyHomePage({Key key, this.title}) : super(key: key);
-//   final String title;
-//   final Map<String, dynamic> pluginParameters = {};
-//   @override
-//   _MyHomePageState createState() => new _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   final formKey = GlobalKey<FormState>();
-//   Future<String> barcodeString;
-// //String barcodeString;
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Scaffold(
-//       appBar: new AppBar(
-//         title: const Text('QRCode Tracking NO.'),
-//         actions: <Widget>[
-//           IconButton(tooltip: 'Upload To Server',
-//           icon: Icon(Icons.cloud_upload),
-//           onPressed: (){
-//             uploadToServer(barcodeString);
-//             print(barcodeString);
-//           },)
-//         ],
-//       ),
-//       body: new Center(
-//           child: new FutureBuilder<String>(
-//               future: barcodeString,
-//               builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-//                 return new Text(snapshot.data != null ? snapshot.data : '');
-//                  print(snapshot.data);
-                 
-//           //uploadToServer(barcodeString);
-//               })),
-//       floatingActionButton: new FloatingActionButton(
-//         onPressed: () {
-//           setState(() {
-//             barcodeString = new QRCodeReader()
-//                 .setAutoFocusIntervalInMs(200)
-//                 .setForceAutoFocus(true)
-//                 .setTorchEnabled(true)
-//                 .setHandlePermissions(true)
-//                 .setExecuteAfterPermissionGranted(true)
-//                 .scan();
-//           });
-//         },
-//         tooltip: 'Reader Tracking NO.',
-//         child: new Icon(Icons.add_a_photo),
-//       ),
-//     );
-//   }
-
-//   void uploadToServer(barcodeString){
-//     print('You Click Upload');
-//     print(formKey.currentState.validate());
-//     formKey.currentState.save();
-//     print('barcode=$barcodeString.data');
-//     sentNewBarcodeToServer(barcodeString);
-//   }
-// void sentNewBarcodeToServer(
-//   String userBarcode) async{
-//     String url =
-//     'http://androidthai.in.th/sun/addDataOill.php?isAdd=true&Barcode=$userBarcode';
-//     var respone = await get(url);
-//     var result = json.decode(respone.body);
-//     print('result ==>$result');
-//     if (result.toString()=='true'){
-//       print('back process');
-//       Navigator.pop(context);
-      
-//     }
-//   }
-// }
-
 import 'dart:async';
 import 'package:barcode_scan/barcode_scan.dart';
+//import 'package:fast_qr_reader_view/fast_qr_reader_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
@@ -111,6 +16,15 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String barcode = "";
+ //int _counter = 0; 
+
+ void _incrementCounter() {
+
+   setState(() {
+    //_counter++;
+   });
+ }
+
 
   @override
   initState() {
@@ -124,22 +38,32 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
           appBar: AppBar(
             title: Text('QR Counter'),
+            actions: <Widget>[IconButton(tooltip: 'upload',
+            icon: Icon(Icons.cloud_upload),onPressed: (){},)],
           ),
           body: Center(
             child: Column(
               children: <Widget>[
                 Container(
-                  child: MaterialButton(
-                      onPressed: scan,
-                      child: Text("Scanning....")),
-                  padding: const EdgeInsets.all(8.0),
+                  
+                  
                 ),
+                
                 Text(barcode),
+               
+
               ],
             ),
-          )),
+          ),
+          floatingActionButton: FloatingActionButton(
+                          onPressed: scan,
+                      child: Icon(Icons.add_a_photo)),
+                  //padding: const EdgeInsets.all(8.0),
+          ),
+
     );
   }
+
 
   Future sendtoserver(data) async {
     print('============');
@@ -156,7 +80,8 @@ class _MyAppState extends State<MyApp> {
     try {
       String barcode = await BarcodeScanner.scan();
       setState(() => this.barcode = barcode);
-      sendtoserver(this.barcode);
+
+      //sendtoserver(this.barcode);
     } on PlatformException catch (e) {
       if (e.code == BarcodeScanner.CameraAccessDenied) {
         setState(() {
@@ -172,6 +97,3 @@ class _MyAppState extends State<MyApp> {
     }
   }
 }
-
-
-
